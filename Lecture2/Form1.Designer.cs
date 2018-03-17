d53b78049ac310997019835a2d69a8e49fe8beb8
Szymon Szczpeański <szymon.szczepanski@gmail.com>
2018-03-17 09:02:04 +0100

    create personal form

--- Lecture2/Form1.Designer.cs
+++ Lecture2/Form1.Designer.cs
@@ -28,12 +28,279 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.btnChangeAdress = new System.Windows.Forms.Button();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.mtxtPhoneNumber = new System.Windows.Forms.MaskedTextBox();
+            this.mtxtEmail = new System.Windows.Forms.MaskedTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pbPhoto = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnUploadPhoto = new System.Windows.Forms.Button();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.dpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 29);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(104, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(306, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(12, 71);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(65, 17);
+            this.lblSurname.TabIndex = 2;
+            this.lblSurname.Text = "Surname";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(104, 68);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(306, 22);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(12, 363);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(52, 17);
+            this.lblAdress.TabIndex = 4;
+            this.lblAdress.Text = "Adress";
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(104, 363);
+            this.txtAdress.Multiline = true;
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(306, 141);
+            this.txtAdress.TabIndex = 5;
+            // 
+            // btnChangeAdress
+            // 
+            this.btnChangeAdress.Location = new System.Drawing.Point(416, 363);
+            this.btnChangeAdress.Name = "btnChangeAdress";
+            this.btnChangeAdress.Size = new System.Drawing.Size(178, 28);
+            this.btnChangeAdress.TabIndex = 6;
+            this.btnChangeAdress.Text = "Change adress";
+            this.btnChangeAdress.UseVisualStyleBackColor = true;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(18, 538);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(101, 17);
+            this.lblPhoneNumber.TabIndex = 7;
+            this.lblPhoneNumber.Text = "Phone number";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(18, 576);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(89, 17);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email adress";
+            // 
+            // mtxtPhoneNumber
+            // 
+            this.mtxtPhoneNumber.Location = new System.Drawing.Point(125, 538);
+            this.mtxtPhoneNumber.Name = "mtxtPhoneNumber";
+            this.mtxtPhoneNumber.Size = new System.Drawing.Size(285, 22);
+            this.mtxtPhoneNumber.TabIndex = 10;
+            // 
+            // mtxtEmail
+            // 
+            this.mtxtEmail.Location = new System.Drawing.Point(113, 576);
+            this.mtxtEmail.Name = "mtxtEmail";
+            this.mtxtEmail.Size = new System.Drawing.Size(297, 22);
+            this.mtxtEmail.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(474, 650);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(567, 650);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // pbPhoto
+            // 
+            this.pbPhoto.Location = new System.Drawing.Point(104, 112);
+            this.pbPhoto.Name = "pbPhoto";
+            this.pbPhoto.Size = new System.Drawing.Size(306, 122);
+            this.pbPhoto.TabIndex = 16;
+            this.pbPhoto.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Photo";
+            // 
+            // btnUploadPhoto
+            // 
+            this.btnUploadPhoto.Location = new System.Drawing.Point(430, 112);
+            this.btnUploadPhoto.Name = "btnUploadPhoto";
+            this.btnUploadPhoto.Size = new System.Drawing.Size(150, 28);
+            this.btnUploadPhoto.TabIndex = 17;
+            this.btnUploadPhoto.Text = "Upload image...";
+            this.btnUploadPhoto.UseVisualStyleBackColor = true;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(18, 263);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(31, 17);
+            this.lblSex.TabIndex = 18;
+            this.lblSex.Text = "Sex";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(104, 262);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 21);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Female";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(205, 262);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 21);
+            this.checkBox2.TabIndex = 20;
+            this.checkBox2.Text = "Male";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(295, 262);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 21);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "Unknow";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // dpDateOfBirth
+            // 
+            this.dpDateOfBirth.Location = new System.Drawing.Point(113, 300);
+            this.dpDateOfBirth.Name = "dpDateOfBirth";
+            this.dpDateOfBirth.Size = new System.Drawing.Size(200, 22);
+            this.dpDateOfBirth.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Date of birth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(654, 716);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dpDateOfBirth);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.btnUploadPhoto);
+            this.Controls.Add(this.pbPhoto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.mtxtEmail);
+            this.Controls.Add(this.mtxtPhoneNumber);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.btnChangeAdress);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Some personal form";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Button btnChangeAdress;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.MaskedTextBox mtxtPhoneNumber;
+        private System.Windows.Forms.MaskedTextBox mtxtEmail;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.PictureBox pbPhoto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUploadPhoto;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.DateTimePicker dpDateOfBirth;
+        private System.Windows.Forms.Label label5;
     }
 }
 
